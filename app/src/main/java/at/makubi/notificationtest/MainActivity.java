4e556fe94afdecb72ad1aa3136d8ebcbe45aa638
S-trace <S-trace@list.ru>
2017-09-27 19:03:00 +0300

    [SWIPES] Send broadcast intents instead of direct launch of intents

--- app/src/main/java/at/makubi/notificationtest/MainActivity.java
+++ app/src/main/java/at/makubi/notificationtest/MainActivity.java
@@ -1,6 +1,5 @@
 package at.makubi.notificationtest;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,24 +22,16 @@ public class MainActivity extends _SwipeActivityClass {
     @Override
     protected void onSwipeRight(float velocity, float distance, float x1, float x2) {
         Log.d("S-trace", "onSwipeRight, velocity=" + velocity + ", distance=" + distance + ", x1=" + x1 + ", x2=" + x2);
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.settings");
-        if (launchIntent != null) {
-            // Start non-default activity from another app:
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.setComponent(new ComponentName("com.android.settings","com.android.settings.accounts.AddAccountSettings"));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);//null pointer check in case package name was not found
-        }
+        Intent intent = new Intent("com.cybertel.SWIPE_RIGHT");
+        intent.putExtra("telno", "123-45678");
+        sendBroadcast(intent);
     }
 
     @Override
     protected void onSwipeLeft(float velocity, float distance, float x1, float x2) {
         Log.d("S-trace", "onSwipeLeft, velocity=" + velocity + ", distance=" + distance + ", x1=" + x1 + ", x2=" + x2);
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.dialer");
-        if (launchIntent != null) {
-            // Start a default launcher activity from another app:
-            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(launchIntent);//null pointer check in case package name was not found
-        }
+        Intent intent = new Intent("com.cybertel.SWIPE_LEFT");
+        intent.putExtra("telno", "123-45678");
+        sendBroadcast(intent);
     }
 }
